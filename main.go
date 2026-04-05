cc729c6b3b54f7449b56202eea12adb497e0e0a7
ccastc02 <ccastc02@estudiantes.unileon.es>
2026-04-05 20:38:49 +0200

    Add health endpoint

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"Flow_gym_go_project/database"
+	"Flow_gym_go_project/handlers"
 
 	"github.com/joho/godotenv"
 )
@@ -22,4 +24,9 @@ func main() {
 	defer db.Close()
 
 	fmt.Println("Database connected successfully")
+
+	http.HandleFunc("/health", handlers.HealthHandler)
+
+	fmt.Println("Server running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
