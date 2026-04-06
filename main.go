71e0f7f71112142bd1a17cb318716a72fb8daf47
ccastc02 <ccastc02@estudiantes.unileon.es>
2026-04-06 12:11:28 +0200

    Add basic homepage for exercise recommendations

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
@@ -24,6 +25,17 @@ func main() {
 	defer db.Close()
 
 	fmt.Println("Database connected successfully")
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("templates/index.html")
+		if err != nil {
+			http.Error(w, "Could not load homepage", http.StatusInternalServerError)
+			return
+		}
+		tmpl.Execute(w, nil)
+	})
 
 	http.HandleFunc("/health", handlers.HealthHandler)
 	http.HandleFunc("/recommendation", handlers.RecommendationHandler(db))
